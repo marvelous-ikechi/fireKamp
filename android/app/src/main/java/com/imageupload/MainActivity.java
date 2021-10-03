4a2cd62fb2b2ea99ad8c76f2d667f857f475e40b
Marvelous <marvelike7@gmail.com>
2021-10-03 19:47:40 +0100

    modified with react navigation requirements

--- android/app/src/main/java/com/imageupload/MainActivity.java
+++ android/app/src/main/java/com/imageupload/MainActivity.java
@@ -1,3 +1,5 @@
+import android.os.Bundle;
+
 package com.imageupload;
 
 import com.facebook.react.ReactActivity;
@@ -8,8 +10,15 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+     super.onCreate(null);
+   }
+
   @Override
   protected String getMainComponentName() {
     return "ImageUpload";
   }
+
+
 }
